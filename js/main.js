7bcef889f1658c8a5400fbf7d54edb74f8b328e9
λhmad λjmi <ahmadajmi@outlook.com>
2014-07-19 17:30:31 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function(){
+  function Editor(input, preview) {
+    this.update = function () {
+      preview.innerHTML = markdown.toHTML(input.value);
+    };
+    input.editor = this;
+    this.update();
+  }
+  var $ = function (id) { return document.getElementById(id); };
+  new Editor($("text-input"), $("preview"));
+})();
