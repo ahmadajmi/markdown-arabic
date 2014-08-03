91c89b0dc972421281f1df612db58dc641b4f8ac
λhmad λjmi <ahmadajmi@outlook.com>
2014-08-04 01:34:29 +0300

    Save written data to localStorage so every thing is written will be saved and retrived if the browser is closed

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ function Editor(input, preview) {
 var $ = function (id) { return document.getElementById(id); };
 new Editor($("text-input"), $("preview"));
 
+// Check if local Storage is supported
+function isLocalStorage() {
+  return !!window.localStorage;
+}
+
+// Save data to the localStorage
+$("text-input").addEventListener('change', function(e){
+  if (isLocalStorage()) {
+    localStorage.copy = this.value;
+  }
+});
+
+// Fetch Data from localStorage if it was saved before
+window.onload = function() {
+  if (isLocalStorage() && localStorage.copy) {
+    $("text-input").innerHTML = localStorage.copy;
+    new Editor($("text-input"), $("preview"));
+  }
+}
+
