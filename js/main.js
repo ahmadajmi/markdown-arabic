198aafcf8bb1f39287d00283c796850eb4c68828
λhmad λjmi <ahmad@ahmadajmi.com>
2014-11-06 20:28:40 +0200

    Code cleaning & formatting

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
+'use strict';
+
 function Editor(input, preview) {
-  this.update = function () {
-    preview.innerHTML = markdown.toHTML(input.value);
-  };
-  input.editor = this;
-  this.update();
+	this.update = function() {
+		preview.innerHTML = markdown.toHTML(input.value);
+	};
+	input.editor = this;
+	this.update();
 }
 
-var $ = function (id) { return document.getElementById(id); };
+var $ = function(id) {
+	return document.getElementById(id);
+};
+
 new Editor($("text-input"), $("preview"));
 
 // Check if local Storage is supported
 function isLocalStorage() {
-  return !!window.localStorage;
+	return !!window.localStorage;
 }
 
 // Save data to the localStorage
-$("text-input").addEventListener('change', function(e){
-  if (isLocalStorage()) {
-    localStorage.copy = this.value;
-  }
+$("text-input").addEventListener('change', function(e) {
+	if (isLocalStorage()) {
+		localStorage.copy = this.value;
+	}
 });
 
 // Fetch Data from localStorage if it was saved before
 window.onload = function() {
-  if (isLocalStorage() && localStorage.copy) {
-    $("text-input").innerHTML = localStorage.copy;
-    new Editor($("text-input"), $("preview"));
-  }
+	if (isLocalStorage() && localStorage.copy) {
+		$("text-input").innerHTML = localStorage.copy;
+		new Editor($("text-input"), $("preview"));
+	}
 }
 
