2d23d29058de5bb8c256175333853a85fea63635
tarrsalah <tarrsalah@gmail.com>
2015-08-18 03:39:51 +0100

    Move to browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
 'use strict';
 
-var gulp      = require('gulp'),
-    uglify       = require('gulp-uglify'),
-    minifyCSS = require('gulp-minify-css'),
-    concat    = require('gulp-concat'),
-    rename    = require('gulp-rename'),
-    stripCssComments = require('gulp-strip-css-comments');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+var stripCssComments = require('gulp-strip-css-comments');
 
+// CSS task
 
-// CSS Task
-
-gulp.task('css', function () {
-  return gulp.src(['public/css/bootstrap.min.css', 'public/css/main.css'])
-  .pipe(minifyCSS())
-  .pipe(stripCssComments())
-  .pipe(concat('style.css'))
-  .pipe(rename('style.min.css'))
-  .pipe(gulp.dest('public/css'));
+gulp.task('css', function() {
+  return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/main.css'])
+    .pipe(minifyCSS())
+    .pipe(stripCssComments())
+    .pipe(concat('style.css'))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('public/css'));
 });
 
+// JavaScript task
 
-// JS Task
-
-gulp.task('js', function(){
-  return gulp.src([
-    'public/js/vendor/markdown.min.js',
-    'public/js/main.js'])
-    .pipe(concat('app.min.js'))
+gulp.task('js', function() {
+  return browserify('./public/js/main.js')
+    .bundle()
+    .pipe(source('appp.js'))
+    .pipe(buffer())
     .pipe(uglify())
-    .pipe(gulp.dest('public/js'))
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('./public/js/'));
 });
 
-
-// Default Task
 gulp.task('default', ['css', 'js']);
